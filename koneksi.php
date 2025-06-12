48796d654df8d5284b9fc7003d295a4f531befc9
satriawanhaykal <satriawan.haykal@gmail.com>
2025-06-12 17:12:01 +0700

    first commit

--- koneksi.php
+++ koneksi.php
@@ -1,18 +1,13 @@
 <?php
-// Konfigurasi koneksi ke database
-$host = "localhost"; // Host MySQL
-$username = "lufiweb_usersmk"; // Username MySQL
-$password = "P4s5Word&"; // Password MySQL
-$database = "lufiweb_smk"; // Nama Database
+// Include file koneksi
+include_once "donoryuk_backend/config/database.php";
 
-// Buat koneksi ke database
-$conn = mysqli_connect($host, $username, $password, $database);
+// Jalankan koneksi
+$conn = mysqli_connect("localhost", "donor424_root", "zq.A8QvR]%Qu_P5=", "donor424_donor_yuk_db");
 
-// Periksa koneksi
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-// Jika koneksi berhasil
-echo "Koneksi ke database berhasil";
+echo "Koneksi ke database berhasil!";
 ?>
