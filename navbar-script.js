ca78e7c55297c4c1e77bd2da4e9d10a58ec12315
satriawanhaykal <satriawan.haykal@gmail.com>
2025-06-12 17:41:40 +0700

    first commit

--- navbar-script.js
+++ navbar-script.js
@@ -9,7 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const donorDropdownBtn = document.getElementById('donor-dropdown-btn');
     const donorDropdownContent = document.getElementById('donor-dropdown-content');
 
-    const BASE_API_URL = 'http://localhost/PemWebTeo/donoryuk_backend/api/';
+    const BASE_API_URL = 'http://donoryuk.xyz/donoryuk_backend/api/';
+
 
     // Function to update navbar based on user login status
     function updateNavbarState() {
